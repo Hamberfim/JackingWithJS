8b3f54aa786ebb713b94798f07fba8b94d14d659
hamberfim <adhamlingithub@gmail.com>
2021-06-07 18:46:01 -0500

    nested array attempt

--- Jacking/employeeObj.js
+++ Jacking/employeeObj.js
@@ -11,7 +11,7 @@ function userData(firstName, lastName, title, department) {
 let empJohnson = new userData('Glen', 'Johnson', 'Manager', 'Sales');
 
 console.log(empJohnson);
-console.log("Type is " + typeof(empJohnson));
+console.log("Type is " + typeof (empJohnson));
 console.log("Mr. " + empJohnson.lastName + " is currently a " + empJohnson.title + " in the " + empJohnson.department + " department.");
 
 
@@ -20,13 +20,98 @@ function findEmployee(employee) {
 }
 
 const empWilliam = {
-    firstName:'Bill', 
-    lastName:'William', 
-    title:'CTO', 
-    department:'Information Technology'
+    firstName: 'Bill',
+    lastName: 'William',
+    title: 'CTO',
+    department: 'Information Technology'
 };
 
 
 let foundEmp = findEmployee(empWilliam);
-console.log("Type is " + typeof(foundEmp));
+console.log("Type is " + typeof (foundEmp));
 console.log(foundEmp);
+
+const empArray = {
+    {
+        employees: {
+            firstName: 'Rob',
+            lastName: 'Jerico',
+            title: 'CFO',
+            department: 'Information Technology'
+        },
+        {
+            firstName: 'Guy',
+            lastName: 'Richey',
+            title: 'Media Manager',
+            department: 'Communications'
+        },
+        {
+            firstName: 'Lisa',
+            lastName: 'Bowenship',
+            title: 'Manager',
+            department: 'Payroll'
+        },
+        {
+            firstName: 'Wendy',
+            lastName: 'Streets',
+            title: 'Copy Editor',
+            department: 'Communications'
+        },
+        {
+            firstName: 'Paul',
+            lastName: 'Docheski',
+            title: 'Assistant Directory',
+            department: 'Human Resources'
+        }
+    }
+
+};
+
+console.log(empArray);
+console.log("Type is " + typeof (empArray));
+let foundBowenship = findEmployee(empArray.employee.lastName);
+console.log(foundBowenship);
+
+var car = {
+
+    manufacturer: {
+
+        toyota: {
+
+            vitz: {
+
+                color: "Blue",
+
+                bodyType: "Hatchback"
+
+            },
+
+            corolla: {
+
+                color: "Silver",
+
+                bodyType: "Sedan"
+
+            }
+        },
+
+        Honda: {
+
+            civic: {
+
+                color: "White",
+
+                bodyType: "Hatchback"
+
+            },
+
+            city: {
+
+                color: "Black",
+
+                bodyType: "Sedan"
+
+            }
+        }
+    }
+};
