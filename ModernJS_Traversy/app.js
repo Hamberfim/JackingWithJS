bfe80bcd0ea81e7db666b95bdd715994e72cfa29
Hamberfim <adhamlingithub@gmail.com>
2021-06-09 14:52:21 -0500

    Traversy-Modern JS

---
+++ ModernJS_Traversy/app.js
@@ -0,0 +1,5 @@
+let elHeaderOne = document.getElementById('headerOne');
+elHeaderOne.textContent = "JS Sandbox: Section 2";
+
+let elTableHolder = document.getElementById("tableHolder");
+elTableHolder.textContent = "I'm inside the div tag.";
