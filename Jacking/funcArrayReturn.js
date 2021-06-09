edf112f325234d564ab86fd3be915a1c661127d4
Hamberfim <adhamlingithub@gmail.com>
2021-06-09 13:10:54 -0500

    added iife

--- Jacking/funcArrayReturn.js
+++ Jacking/funcArrayReturn.js
@@ -1,4 +1,4 @@
-// array reteun from a function
+// array return from a function
 "use strict";
 
 function catUserName(fName, mName, lName) {
@@ -24,3 +24,39 @@ let guestVerOne = guestName("John", "Hall")[0];
 let guestVerTwo = guestName("John", "Michael", "Hall")[1];
 console.log("Guest's name: " + guestVerOne);
 console.log("Guest's full name: " + guestVerTwo);
+
+// immediately invoked function expressions (iife 'iffy') 
+// often used in event handlers, listeners, 
+// to prevent conflicts where same variable names are used, 
+// as a wrapper around some code and popular in jQuery.
+let personsName = (function() {
+    let name = "Anthony" + " " + "Bourdain";
+    let fullName = "Anthony" + " " + "Michael" + " " + "Bourdain";
+    return [name, fullName];
+}());  // parenth.() call the func immediately
+
+let personOne = personsName[0];
+let personTwo = personsName[1];
+console.log("Name: " + personOne);
+console.log("Full name: " + personTwo);
+
+// anonymous array function
+let paint  = function(width, height, isWall) {
+    let area  =  width * height;
+    let msg = "";
+    if(isWall) {
+        msg = "is the area and since it's a wall you can paint it.";
+        return [area, msg];
+    }
+    else {
+        msg = "is the area, but it's not a wall so you shouln't paint it.";
+        return [area, msg];
+    }
+
+}
+
+let surfaceOne = paint(12, 22, true);
+console.log(`${surfaceOne[0]} ${surfaceOne[1]}`);
+
+let surfaceTwo = paint(24, 15, false);
+console.log(`${surfaceTwo[0]} ${surfaceTwo[1]}`);
