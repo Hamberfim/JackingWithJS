dc2936017ee67e726ba9dbbdbff989bde1e92ee7
Hamberfim <adhamlingithub@gmail.com>
2021-06-09 19:47:33 -0500

    js tag textcontent replacement

--- ModernJS_Traversy/app.js
+++ ModernJS_Traversy/app.js
@@ -3,3 +3,7 @@ elHeaderOne.textContent = "JS Sandbox: Section 2";
 
 let elTableHolder = document.getElementById("tableHolder");
 elTableHolder.textContent = "I'm inside the div tag.";
+
+let elPara = document.getElementById("para");
+elPara.textContent = "I'm inside a paragraph tag. A bunch of copy no one cares about. A bunch of copy no one cares about. A bunch of copy no one cares about. A bunch of copy no one cares about. A bunch of copy no one cares about. A bunch of copy no one cares about. A bunch of copy no one cares about. ";
+
