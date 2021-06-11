40e575045133b9210a75ab85370aaa364d52bfe9
Hamberfim <adhamlingithub@gmail.com>
2021-06-11 11:39:54 -0500

    seting masthead

--- Jacking/docWrite.js
+++ Jacking/docWrite.js
@@ -1,1 +1,9 @@
 "use strict";
+
+let masthead = "Miles Per Gallon";
+// populate masthead
+let elMasthead = document.getElementById('masthead');
+elMasthead.textContent = `${masthead}`;
+
+// prompt user
+// get miles driven
