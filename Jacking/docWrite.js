05099ebf7c1ce8a970b39b410b7eace6f00f4abc
Hamberfim <adhamlingithub@gmail.com>
2021-06-11 12:03:09 -0500

    adjusted templat and script

--- Jacking/docWrite.js
+++ Jacking/docWrite.js
@@ -7,8 +7,14 @@ elMasthead.textContent = `${masthead}`;
 
 // prompt user - converting to float
 // get miles driven 
-const userMiles = parseFloat(prompt('Enter the number of miles driven: '));
+const userMiles = parseFloat(prompt('Enter the number of miles driven: ', 0));
 // get gallons used
-const galUsed = parseFloat(prompt('Enter the number of gallons used: '));
+const galUsed = parseFloat(prompt('Enter the number of gallons used: ', 0));
 // calculate mpg
 const mpg = parseFloat(userMiles / galUsed);
+
+// populate label area of html doc
+let elMiles = document.getElementById('miles');
+elMiles.textContent = userMiles;
+let elGallons = document.getElementById('gallons');
+let elMPG = document.getElementById('mpg');
