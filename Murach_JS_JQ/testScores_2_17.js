5f65e111c31fe573cda6ca1b072a77e510932bfb
Hamberfim <adhamlingithub@gmail.com>
2021-06-11 12:36:13 -0500

    script template

--- Murach_JS_JQ/testScores_2_17.js
+++ Murach_JS_JQ/testScores_2_17.js
@@ -1,2 +1,21 @@
 "use strict";
 // get user input and calculate the average score
+let total = 0;
+
+const score1 = parseFloat(prompt("Enter first of three test scores: "));
+total += score1;
+
+const score2 = parseFloat(prompt("Enter second of three test scores: "));
+total += score2;
+
+const score3 = parseFloat(prompt("Enter third of three test scores: "));
+total += score3;
+
+// calculate average
+const average = parseFloat(total / 3);
+
+// gather html content
+const html = ``;
+
+// write html content
+document.write();
