cda1f4b79f45ca82ca9691b5f727b1e397746a78
Hamberfim <adhamlingithub@gmail.com>
2021-06-24 16:00:15 -0500

    grams to cups convert added

--- Jacking/strManip.js
+++ Jacking/strManip.js
@@ -1,9 +1,9 @@
 "use strict";
 
+// function to count and display the number of characters
 function displayCount() {
 
     let word = document.getElementById('word').value;
-    // elJob.textContent = job;
 
     let phrase = word + " has XX characters.";
     let newPhrase = "";
@@ -16,4 +16,22 @@ function displayCount() {
     let elPhrase = document.getElementById('phrase');
     elPhrase.textContent = newPhrase;
 
+}
+
+// function to calculate and display grams in cups
+function convertGramsToCups() {
+    let grams = document.getElementById('grams').value;
+
+    let cups = grams / 125.00;  // 120-130 grams equals a 'light' cup measurement
+    cups = cups.toFixed(2);
+
+    let converted = grams + " gram(s) is approximately XX cup(s).";
+    let cupResults = "";
+
+    if(converted.includes('XX')) {
+        cupResults = converted.replace('XX', cups);
+    }
+
+    let elResults = document.getElementById('results');
+    elResults.textContent = cupResults;
 }
