8fbb8508a3b2510a03457adccbbe28014c155122
Anthony <adhamlingithub@gmail.com>
2021-07-30 11:58:48 -0500

    mod script slightly

--- Jacking/strManip.js
+++ Jacking/strManip.js
@@ -5,12 +5,14 @@ function displayCount() {
 
     let word = document.getElementById('word').value;
 
-    let phrase = word + " has XX characters.";
+    let myBr = '<br>';
+
+    let phrase = word + " <-- has XX characters (not including spaces).";
     let newPhrase = "";
 
     if(phrase.includes('XX')) {
         word = word.split(" ").join("");  // remove space to count only letters
-        newPhrase = phrase.replace('XX', word.length);
+        newPhrase = phrase.replace('XX', `${word.length}`);
         
     }
     let elPhrase = document.getElementById('phrase');
