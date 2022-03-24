9d8602baca2c089caf7fc44f3b0aa9f2bb306bf0
Hamberfim <adhamlingithub@gmail.com>
2022-03-24 17:17:32 -0500

    primitive types and methods of oop

--- OOP_javascript/_01_primTypes.js
+++ OOP_javascript/_01_primTypes.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// declare and initialize
 let myName = "ANTHONY";
 let lowercaseName = myName.toLowerCase();
 let firstLetter = myName.charAt(0);
@@ -7,6 +8,7 @@ let nameLength = myName.length;
 let middleIndex = nameLength / 2;
 let middleOfName = myName.substring(middleIndex - 1, middleIndex + 2);
 
+// display
 let elMyName = document.getElementById('myName');
 elMyName.textContent = myName;
 
@@ -19,6 +21,7 @@ elFirstLetter.textContent = firstLetter;
 let elMiddleOfName = document.getElementById('middleOfName');
 elMiddleOfName.textContent = middleOfName;
 
+// declare and initialize
 let myIntCount = 12;
 let fixToDoubleCount = myIntCount.toFixed(2);
 
@@ -27,3 +30,11 @@ elMyIntCount.textContent = myIntCount;
 
 let elFixToDoubleCount = document.getElementById('fixToDoubleCount');
 elFixToDoubleCount.textContent = fixToDoubleCount;
+
+// declare and initialize
+let flagTrue = true;
+let strFlag = flagTrue.toString();
+
+// display
+let elStrFlag = document.getElementById('strFlag');
+elStrFlag.textContent = strFlag;
